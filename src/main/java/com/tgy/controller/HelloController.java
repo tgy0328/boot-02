5ad97ba4b3d8c6dd7d836ba9f2d37701ab7e7c53
Tgy <1393305714@qq.com>
2023-01-05 10:13:25 +0800

    second commit

--- src/main/java/com/tgy/controller/HelloController.java
+++ src/main/java/com/tgy/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.tgy.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,13 @@ public class HelloController {
     @RequestMapping("/bug.jpg")
     public String hello(){
         return "aaa";
+    }
+
+    @RequestMapping("/hello/{username}")
+    public String helloTest(@PathVariable("username") String username){
+
+        System.out.println("你好："+username);
+        return "你好："+username;
+
     }
 }
