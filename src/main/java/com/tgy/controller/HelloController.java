48e71213c8724a23fa502a2981d30d819aa037d0
Tgy <1393305714@qq.com>
2023-01-05 13:56:26 +0800

    push commit

--- src/main/java/com/tgy/controller/HelloController.java
+++ src/main/java/com/tgy/controller/HelloController.java
@@ -27,6 +27,7 @@ public class HelloController {
         System.out.println("hello git");
         System.out.println("master test");
         System.out.println("hot-fix test!");
+        System.out.println("push test");
         return "你好：" + username;
     }
     @RequestMapping("/test")
